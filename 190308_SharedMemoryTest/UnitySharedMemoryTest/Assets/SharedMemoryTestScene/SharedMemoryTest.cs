9468179f2b3adea0bbf96f4df975d9b56ceb3bbe
9ballsyndrome <9.ball.syndrome@gmail.com>
2019-03-08 05:01:45 +0900

    Added shared memory test for webgl2-compute

---
+++ 190308_SharedMemoryTest/UnitySharedMemoryTest/Assets/SharedMemoryTestScene/SharedMemoryTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class SharedMemoryTest: MonoBehaviour
+{
+    public ComputeShader computeShader;
+    int kernelIndex;
+    ComputeBuffer resultBuffer;
+
+    void Start()
+    {
+        int numThreadsPerGroup = 512;
+        int numGroups = 1;
+        int numTotalThreads = numThreadsPerGroup * numGroups;
+
+        this.kernelIndex = this.computeShader.FindKernel("main");
+
+        this.resultBuffer = new ComputeBuffer(numTotalThreads, sizeof(float), ComputeBufferType.Raw);
+        this.computeShader.SetBuffer(this.kernelIndex, "_ssbo", this.resultBuffer);
+
+        this.computeShader.Dispatch(this.kernelIndex, numGroups, 1, 1);
+
+        float[] result = new float[numTotalThreads];
+        // There are no need to barrier, because GetData waits for GPU completion
+        this.resultBuffer.GetData(result);
+
+        Debug.Log("complete compute");
+
+        int correctResult = (numThreadsPerGroup - 1) * numThreadsPerGroup / 2;
+        for (int i = 0; i < numTotalThreads; i++)
+        {
+            if(result[i] != correctResult)
+            {
+                Debug.Log("check error: " + i + ", " + result[i]);
+            }
+        }
+
+        Debug.Log("complete check errors");
+
+        this.resultBuffer.Release();
+    }
+}
